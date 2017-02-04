358fd8b4f65d9144de947c5e137687294dec5564
Dexter Taylor <dexter.taylor@tictail.com>
2017-02-04 13:11:12 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <hello></hello> -->
+    <h3>Hello from HELL</h3>
+    <testcomp/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Testcomp from './components/Testcomp'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Testcomp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="test">
+       <p>This is a paragraph block</p>
+       <select name = "selector">
+	<option value ='1'>Foo</option>
+	<option value ='2'>Bar</option>
+	<option value ='3'>Bazz</option>	
+       </select>
+  </div>
+</template>
+
